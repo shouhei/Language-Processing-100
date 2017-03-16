d63e1ef86b86b376b526ec670b98f0e2dc6ff9cc
山口将平 <shouhei.yamaguchi.be@gmail.com>
2017-03-17 00:44:00 +0900

    bug fix

--- five/src/Lib.hs
+++ five/src/Lib.hs
@@ -9,7 +9,9 @@ validate s = if last s == ',' || last s == '.' then init s else s
 someFunc :: IO ()
 someFunc = do
   let t = Prelude.map validate $ words "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
-  let def = [2, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 1, 1, 2, 1]
-  let dict = Map.fromList $ Prelude.map (\(a, b) -> ((take a b),b)) $ zip def t
+  let def = [1, 2, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 1, 1, 2, 1]
+  let shorts = Prelude.map (\(a, b) -> if length b > 1 then (take a b) else b) $ zip def t
+  let n = [1 .. length shorts]
+  let dict = Map.fromList $ zip shorts n
   print dict
   print $ Map.lookup "H" dict
