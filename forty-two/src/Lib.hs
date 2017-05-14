9bebd657e1740e6db9f4d8389d1e3bb0cdf02df0
山口将平 <shouhei.yamaguchi.be@gmail.com>
2017-05-14 23:12:28 +0900

    Add forty two

---
+++ forty-two/src/Lib.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE OverloadedStrings, QuasiQuotes, FlexibleContexts, DeriveGeneric, ScopedTypeVariables #-}
+module Lib
+    ( someFunc
+    ) where
+
+import qualified Text.CaboCha as CaboCha
+import Text.Regex.PCRE.Heavy
+import Data.List
+import qualified Data.List.Split as LS
+
+data Morph = Morph { surface::String, base::String, pos::String, pos1::String} deriving Show
+data Chunk = Chunk { morphs::[Morph], dst::Int, srcs::[Int]} deriving Show
+
+chunkToStr :: Chunk -> String
+chunkToStr x = foldl (\y z -> y ++ (surface z)) "" (morphs x)
+
+chunkDstsToStr :: [Chunk] -> [String]
+chunkDstsToStr x = do
+  let srcsData = map (\y -> dst y) x
+  map (\y ->
+         if y == -1 then
+             ""
+         else
+            chunkToStr (x !! y)
+      ) srcsData
+
+makeMorphList :: [String] -> [Morph]
+makeMorphList [] = []
+makeMorphList (x:xs) = do
+  let l = split [re|(\t|,)|] x
+  if ((length l) < 7)  then
+        makeMorphList xs
+  else
+      Morph {surface = (l !! 0), base = (l !! 7), pos = (l !! 1), pos1 = (l !! 2) } : makeMorphList xs
+
+dstToInt :: String -> Int
+dstToInt x
+  | (x !! 0) == '-' = -1
+  | otherwise       = read $ init x
+
+dstToSrc :: [Int] -> [[Int]]
+dstToSrc x = map (\y -> elemIndices y x) [0..(length x)]
+
+metaToDst :: String -> Int
+metaToDst x = dstToInt  $ (words x) !! 2
+
+makeChunks :: [String] -> [[Chunk]]
+makeChunks [] = []
+makeChunks (x:xs) = do
+  let metas = dstToSrc $ map (\y -> dstToInt ((words y) !! 2)) $ filter (\y -> y !! 0 == '*') $ lines $ x
+  let morphLines = tail $ LS.splitWhen (\y -> y !! 0 == '*') $ lines x
+  (map (\y -> makeChunk (fst y) (metaToDst ((lines x) !! 0)) (snd y)) $ zip morphLines metas): (makeChunks xs)
+
+makeChunk :: [String] -> Int -> [Int] -> Chunk
+makeChunk x y z = do
+  let m = makeMorphList $ x
+  Chunk{morphs=m, dst=y, srcs=z}
+
+printChunkWithDst :: [Chunk] -> IO()
+printChunkWithDst x = do
+  let bases = map chunkToStr x
+  let dsts = chunkDstsToStr x
+  mapM_ putStrLn $ map (\y -> (fst y) ++ "\t" ++(snd y)) $ zip bases dsts
+
+someFunc :: IO ()
+someFunc = do
+  text <- readFile "neko.txt"
+  cabocha  <- CaboCha.new ["cabocha", "-f1"]
+  chunkStr <- mapM (\x -> CaboCha.parse cabocha x) (lines text)
+  let chunks = makeChunks chunkStr
+  mapM_ printChunkWithDst chunks
